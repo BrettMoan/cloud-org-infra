b7f9af42437572af527eef14928517368a16f222
Brett Moan <6159590+BrettMoan@users.noreply.github.com>
2026-01-11 21:05:43 -0700

    format

--- main.tf
+++ main.tf
@@ -1,32 +1,32 @@
 locals {
-  repos = jsondecode(file("${path.module}/repos.json"))
+  repos    = jsondecode(file("${path.module}/repos.json"))
   policies = jsondecode(file("${path.module}/policies.json"))
 }
 
 resource "aws_iam_openid_connect_provider" "github" {
-  url = "https://token.actions.githubusercontent.com"
-  client_id_list = ["sts.amazonaws.com"]
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
 
 resource "aws_iam_role" "lambda_exec" {
-	name = "lambda_exec_role"
-	assume_role_policy = jsonencode({
-		Version = "2012-10-17"
-		Statement = [{
-			Action = "sts:AssumeRole"
-			Effect = "Allow"
-			Principal = {
-				Service = "lambda.amazonaws.com"
-			}
-		}]
-	})
+  name = "lambda_exec_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
-	role       = aws_iam_role.lambda_exec.name
-	policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 
